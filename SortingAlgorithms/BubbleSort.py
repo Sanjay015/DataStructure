9c411e474a227774d07eab552bf35f53261396cd
Sanjay Yadav <sanjay.cc.in@gmail.com>
2022-08-13 08:45:16 +0530

    ADD: Added sorting and searching algorithms

---
+++ SortingAlgorithms/BubbleSort.py
@@ -0,0 +1,13 @@
+class BubbleSort:
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def sort(self):
+        pass
---
+++ SortingAlgorithms/QuickSort.py
@@ -0,0 +1,13 @@
+class QuickSort:
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def sort(self):
+        pass
