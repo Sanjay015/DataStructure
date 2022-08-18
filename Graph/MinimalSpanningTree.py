21c11b6b7bc1bc6b91bcded5accf8c260ea85524
Sanjay Yadav <sanjay.cc.in@gmail.com>
2022-08-18 16:48:55 +0530

    UPDATE: Added kruskal algo

--- Graph/MinimalSpanningTree.py
+++ Graph/MinimalSpanningTree.py
@@ -1,4 +1,5 @@
 from PriorityQueue.MinHeap import MinHeap
+from ADT import ADTDisjointSet
 
 
 class Vertex:
@@ -78,6 +79,9 @@ class Graph:
         self._directed_graph = directed
 
     def __iter__(self):
+        return next(self)
+
+    def __next__(self):
         for item in self.vertices.values():
             yield item
 
@@ -112,11 +116,28 @@ class Graph:
                 edges.append((vertex.get_id(), connection.get_id()))
         return edges
 
-    def prims_algorithm(self, start_vertex):
-        # Set the distance for the start node to zero
-        start = self.get_vertex(start_vertex)
-        start.distance = 0
+    def prims_algorithm_route(self):
+        path = ''
+        for node in self:
+            if node.distance != 0:
+                path = '{}Vertex({}) --> Vertex({}), Cost: {}\n'.format(
+                    path, node.previous.get_id(), node.get_id(), node.distance)
+        return path
 
+    def _prims_algorithm_cost(self):
+        cost = 0
+        for node in self:
+            cost += node.distance
+        return cost
+
+    def prims_algorithm(self, start_vertex=None):
+        """Time complexity: O(V ^ 2)"""
+        # Set the distance for the start node to zero
+        if start_vertex is None:
+            start = next(iter(self))
+        else:
+            start = self.get_vertex(start_vertex)
+        start.distance = 0
         # Put tuple pair into min heap
         min_heap = MinHeap()
         for item in self:
@@ -124,16 +145,17 @@ class Graph:
         min_heap.min_heapify()
         unvisited = min_heap.heap[1:]
 
-        # while not queue.is_empty():
         while len(unvisited):
             current_vertex = min_heap.get_min()
             current_vertex.visited = True
 
             for neighbor in current_vertex.get_connections():
-                # if visited do not bother
-                if neighbor.visited:
-                    continue
                 distance = current_vertex.get_weight(neighbor)
+                if neighbor.visited:
+                    if neighbor.distance > distance:
+                        neighbor.distance = distance
+                        neighbor.previous = current_vertex
+                    continue
 
                 if distance < neighbor.distance:
                     neighbor.distance = distance
@@ -149,30 +171,81 @@ class Graph:
                     min_heap.insert(item)
             min_heap.min_heapify()
 
-    def kruskal_algorithm(self, start_vertex):
+        return self._prims_algorithm_cost()
+
+    def kruskal_algorithm(self):
+        """Kruskal's algorithm Steps:
+            1. Sort all the edges in non-decreasing order of their weight.
+            2. Pick the smallest edge. Check if it forms a cycle with the
+               spanning tree formed so far. If cycle is not formed,
+               include this edge. Else, discard it.
+            3. Repeat step#2 until there are (V-1) edges in the spanning tree.
+
+        Time Complexity: O(ELogE + ELogV)
+        """
         edges = []
+        nodes = []
         for vertex in self:
-            pass
+            nodes.append(vertex.get_id())
+            for neighbor in vertex.get_connections():
+                weight = vertex.get_weight(neighbor)
+                vid = vertex.get_id()
+                nid = neighbor.get_id()
+                edges.append((weight, vid, nid))
+
+        # Sort edges by their weight
+        edges = sorted(edges, key=lambda item: item[0])
+
+        adt = ADTDisjointSet(nodes)
+        minimum_spanning_tree = set()
+        cost = 0
+        for edge in edges:
+            weight, source_vertex, destination_vertex = edge
+            if adt.find(source_vertex) != adt.find(destination_vertex):
+                cost += weight
+                adt.union(source_vertex, destination_vertex)
+                minimum_spanning_tree.add(edge)
+
+        mst = ''
+        for _edge in minimum_spanning_tree:
+            mst = '{}Source({}) -> Destination({}), Cost: {}\n'.format(
+                mst, _edge[1], _edge[2], _edge[0]
+            )
+
+        return {
+            'cost': cost,
+            'minimum_spanning_tree': mst,
+        }
 
 
 if __name__ == '__main__':
 
-    g = Graph(directed=True)
+    g = Graph()
     g.add_vertex('a')
     g.add_vertex('b')
     g.add_vertex('c')
     g.add_vertex('d')
     g.add_vertex('e')
+    g.add_vertex('f')
+    g.add_vertex('g')
+    g.add_vertex('h')
+    g.add_vertex('i')
 
-    g.add_edge('a', 'b', -1)
-    g.add_edge('a', 'c', 4)
-    g.add_edge('b', 'c', 3)
-    g.add_edge('b', 'd', 2)
-    g.add_edge('b', 'e', 2)
-    g.add_edge('d', 'c', 5)
-    g.add_edge('d', 'b', 1)
-    g.add_edge('e', 'd', -3)
+    g.add_edge('a', 'b', 4)
+    g.add_edge('a', 'h', 8)
+    g.add_edge('b', 'h', 11)
+    g.add_edge('b', 'c', 8)
+    g.add_edge('c', 'd', 7)
+    g.add_edge('c', 'i', 2)
+    g.add_edge('c', 'f', 4)
+    g.add_edge('d', 'e', 9)
+    g.add_edge('d', 'f', 14)
+    g.add_edge('e', 'f', 10)
+    g.add_edge('g', 'f', 2)
+    g.add_edge('g', 'i', 6)
+    g.add_edge('g', 'h', 1)
+    g.add_edge('h', 'i', 7)
 
-    g.prims_algorithm('a')
-    for v, val in g.vertices.items():
-        print(v, val)
+    result = g.kruskal_algorithm()
+    print(result['cost'])
+    print(result['minimum_spanning_tree'])
