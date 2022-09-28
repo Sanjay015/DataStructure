94c768da249269b28a32cf53d70954a0926ae04f
Sanjay Yadav <sanjay.cc.in@gmail.com>
2022-09-28 14:09:51 +0530

    Update Tries.py

--- Tries/Tries.py
+++ Tries/Tries.py
@@ -19,7 +19,7 @@ class Trie:
             current_node = current_node.children[character]
         current_node.is_end_of_word = True
 
-    def search(search, word):
+    def search(self, word):
         if word == '':
             return True
 
@@ -41,6 +41,29 @@ class Trie:
             current_node = current_node[element]
         return True
 
+    def _walk(self, node, word, word_list):
+        if node.children:
+            for element in node.children:
+                new_word = word + element
+                if node.children[element].is_end_of_word:
+                    word_list.append(new_word)
+                self._walk(node.children[element], new_word, word_list)
+
+    def auto_complete(self, partial_word):
+        current_node = self.root
+        words_list = []
+        for word in partial_word:
+            if word in current_node.children:
+                current_node = current_node.children[word]
+            else:
+                return words_list
+
+        if current_node.is_end_of_word:
+            words_list.append(partial_word)
+
+        self._walk(current_node, partial_word, words_list)
+        return words_list
+
 
 if __name__ == '__main__':
     trie = Trie()
@@ -49,4 +72,3 @@ if __name__ == '__main__':
     trie.add_word('waiter')
     trie.add_word('shop')
     trie.add_word('shopper')
-
